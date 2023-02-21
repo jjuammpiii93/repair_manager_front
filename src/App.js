e53a03be145578e1ac6051031263ff593d985a06
juan gimenez <jigiemenez@gylgroup.com>
2023-02-21 15:52:48 -0300

    se agrega fetch busqueda de ticket

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const url = 'http://localhost:8080/api/v1/ticket'
-  const ticketList = async () => {
+  const [ticket, setTicket]= useState()
+  const fetchTicket = async () => {
     const response = await fetch (url)
+    const responseJSON = await response.json()
+    setTicket (responseJSON)
     console.log(response)
       }
       useEffect (() => {
-        ticketList()
+        fetchTicket()
       })
   return (
     <div className="App">
+      <ul>
+        {!ticket ? 'Cargando....': 
+          ticket.map((ticket , index)=>{
+            return <li>{ticket.description}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
