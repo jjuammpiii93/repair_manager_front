ba2cae553b7ad57fcfb62083213708f107b1a364
juan gimenez <jigiemenez@gylgroup.com>
2023-02-21 15:20:27 -0300

    limpieza de codigo

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 # repair_manager_front
-=======
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -71,4 +70,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
->>>>>>> 09c17ce (Initialize project using Create React App)
+
+> > > > > > > 09c17ce (Initialize project using Create React App)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const url = 'http://localhost:8080/api/v1/ticket'
+  const ticketList = async () => {
+    const response = await fetch (url)
+    console.log(response)
+      }
+      useEffect (() => {
+        ticketList()
+      })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
